52aa91d065fe5727167fe9c69e8b1bfe7de43948
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-24 18:47:58 -0300

    fix commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 getData()
 async function getData(){
-    const response = await fetch("http://127.0.0.1:5500/data.json");
+    const response = await fetch("http://127.0.0.1:5500/data/data.json");
     const json = await response.json();
     recorrerData(json);
 }
